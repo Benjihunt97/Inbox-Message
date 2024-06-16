9082d8034a339d07bbff8a7b20b8b790a6526c5e
Benjihunt97 <73445088+Benjihunt97@users.noreply.github.com>
2024-06-16 10:29:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import Inbox from './components/inbox.js';
+import InboxItem from './components/InboxItem';
+import MessageDisplay from './components/messageBox';
+import Inbox from './components/inbox';
 
 const App = () => {
+  const [selectedMessage, setSelectedMessage] = useState(null);
+  const [inboxVisible, setInboxVisible] = useState(false);
+
+  const handleItemClick = (item) => {
+    setSelectedMessage(item);
+    setInboxVisible(false);  // Hide inbox when a message is selected
+  };
+
+  const toggleInbox = () => {
+    setInboxVisible(!inboxVisible);
+  };
+
   return (
-    <div>
-      <Inbox />
+    <div className="app grid sm:grid-cols-[40%_auto]">
+      <Inbox isVisible={inboxVisible} handleItemClick={handleItemClick} />
+      <div className="message-display">
+        <MessageDisplay 
+          selectedMessage={selectedMessage} 
+          toggleInbox={toggleInbox} 
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 ReactDOM.render(
   <App />,
