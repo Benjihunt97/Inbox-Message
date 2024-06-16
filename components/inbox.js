b7242b9dcd1054ccb047c29a8f40dc520de90f76
Benjihunt97 <73445088+Benjihunt97@users.noreply.github.com>
2024-06-16 10:27:54 +0100

    Update inbox.js

--- components/inbox.js
+++ components/inbox.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import InboxItem from './InboxItem';
 
-const InboxMessages = [
+export const InboxMessages = [
   {
     img: 'https://shismqklzntzxworibfn.supabase.co/storage/v1/object/public/avatars/9869ee0c-d5b8-48c3-a2b9-3feb4bc6fbf7.png',
     name: 'Benji',
-    msg: 'This is a template message and sill be cuz off at a certain width at somepoint. Remember to import your react components.',
+    msg: 'This is a template message and will be cut off at a certain width at some point. Remember to import your react components.',
     tag: '2',
     time: '08:00'
   },
@@ -17,42 +18,19 @@ const InboxMessages = [
   }
 ];
 
-const Inbox = () => {
+const Inbox = ({ isVisible, handleItemClick }) => {
   return (
-    <div className='bg-purple-600 w-72 h-screen text-white overflow-hidden'>
+    <div id='inbox' className={`absolute sm:static top-0 ${isVisible ? 'left-0' : '-left-full'} bg-purple-600 w-72 h-screen sm:h-full text-white overflow-hidden transition-all duration-500 z-50`}>
       <div className='grid grid-cols-[20%_60%_20%] place-items-center w-full h-14 bg-purple-800 text-purple-200 shadow-lg shadow-purple-800'>
         <i className='fa fa-search w-full h-full grid place-items-center'></i>
         <input className='w-full bg-transparent text-sm focus:outline-0 text-white' type='search' placeholder='Search' />
         <i className='fa fa-address-book bg-purple-500 text-lg text-purple-200 w-full h-full grid place-items-center'></i>
       </div>
       <div>
-        <InboxItem />
+        <InboxItem handleClick={handleItemClick} />
       </div>
     </div>  
   );
-}
-
-const InboxItem = () => {
-  return (
-    InboxMessages.map(item => (
-      <div key={item} className='flex gap-3 p-5 overflow-hidden transition-all duration-300 hover:bg-purple-700 hover:shadow-md hover:shadow-purple-800'>
-        <img className='size-14 rounded-full border-2 border-purple-500 shrink-0 object-cover' src={item.img}/>
-        <div className=''>
-          <h3 className='flex items-center gap-3 font-bold'>{item.name}
-            {item.tag ? (
-              <span className='grid place-items-center size-4 bg-pink-500 rounded-full text-xs'>
-                {item.tag}
-              </span>
-            ) : null}
-          </h3>
-          <p className='text-xs clipped-line'>
-            {item.msg}
-          </p>
-        </div>
-        <p className='text-xs text-purple-200'>{item.time}</p>
-      </div>
-    ))
-  )
 }
 
 export default Inbox;
